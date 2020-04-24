a7971fe65ece29b3150233f6d585eb9667d277bc
Pangilinank <40500163+Pangilinank@users.noreply.github.com>
2020-04-24 16:36:21 -0400

    Add files via upload

---
+++ precache-manifest.6940d7d5b6ae4fc879ee5c69a9680174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352e7f24b9ec8411a84f25679d280c08",
+    "url": "/movie_search/index.html"
+  },
+  {
+    "revision": "99088245c7ca06ca2d6e",
+    "url": "/movie_search/static/css/main.516d9558.chunk.css"
+  },
+  {
+    "revision": "822b0599939453261c6b",
+    "url": "/movie_search/static/js/2.7c323fc0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_search/static/js/2.7c323fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99088245c7ca06ca2d6e",
+    "url": "/movie_search/static/js/main.66e628bc.chunk.js"
+  },
+  {
+    "revision": "415a944de73065eec48c",
+    "url": "/movie_search/static/js/runtime-main.a89e2274.js"
+  }
+]);
